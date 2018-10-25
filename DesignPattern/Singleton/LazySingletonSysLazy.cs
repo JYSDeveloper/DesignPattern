eb5136b1aee550bc0ffc84564f3bb86a02335b4f
姜小骚 <826420016@qq.com>
2018-10-25 23:19:01 +0800

    Create By System Lazy Class

---
+++ DesignPattern/Singleton/LazySingletonSysLazy.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * CreateBy:  Alex.Jiang
+ *
+ * DateTime: 10/25/2018 11:12:48 PM
+ *
+ * Contact by WeChat & QQ: 826420016  
+ * Blog:  https://www.jianshu.com/u/fb4e636c3c21
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class LazySingletonSysLazy
+    {
+        private static readonly Lazy<LazySingletonSysLazy> instance = new Lazy<LazySingletonSysLazy>(()=>new LazySingletonSysLazy());
+
+        private LazySingletonSysLazy()
+        { }
+        public LazySingletonSysLazy GetInstance() { return instance.Value; }
+    }
+}
