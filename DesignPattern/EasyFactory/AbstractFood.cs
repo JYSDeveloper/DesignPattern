edbc2d15ed33e9685d98ae07713168b38aa8e86f
jiangyusheng <jiangyusheng@WINDOWS-FPIDAS5>
2018-10-29 18:07:24 +0800

    Easy Factory Pattern Part I

---
+++ DesignPattern/EasyFactory/AbstractFood.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * CreateBy:  Alex.Jiang
+ *
+ * DateTime: 10/25/2018 10:53:20 PM
+ *
+ * Contact by WeChat & QQ: 826420016  
+ * Blog:  https://www.jianshu.com/u/fb4e636c3c21
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyFactory
+{
+    public abstract class AbstractFood
+    {
+        public double Price { get; set; }
+        public string Name { get; set; }
+        public abstract string Destription();
+    }
+}
---
+++ DesignPattern/EasyFactory/ICookFood.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * CreateBy:  Alex.Jiang
+ *
+ * DateTime: 10/25/2018 10:53:20 PM
+ *
+ * Contact by WeChat & QQ: 826420016  
+ * Blog:  https://www.jianshu.com/u/fb4e636c3c21
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyFactory
+{
+    public interface ICookFood
+    {
+        AbstractFood CookFood();
+    }
+}
